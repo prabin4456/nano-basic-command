dc7aaf378b7d89b870fc8d3133032dea11eb837c
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-26 18:39:30 +0545

    Add files via upload

---
+++ nano time date.py
@@ -0,0 +1,25 @@
+nano timedate.sh
+=> opend your nano editor
+
+#!/bin/bash
+apt-get update
+time
+date
+cal
+restart
+
+ctrl X
+=> save coding file
+
+ls
+=> ls mai date.sh file show hoga
+
+chmod +x timedate.sh
+=> termux run karne k liye
+
+sh timedate.sh
+=> run your coding termux
+
+rm timedate.sh
+=> nano date.sh file remove delete garn 
+
