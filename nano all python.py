6cbf676d4dfb2ec5b91907caa89d242966d41ace
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-26 19:03:02 +0545

    Add files via upload

---
+++ nano all python.py
@@ -0,0 +1,20 @@
+# nano run your python code easy termux
+
+pkg install nano
+
+nano tebal.py
+
+i = 0
+while i < 101:
+	print(i, end = " ")
+	i +=1
+	if i == 5000:
+		print(0)
+		
+ctrl X save
+
+ls
+
+python tebal.py
+
+# run your python code easy termux
